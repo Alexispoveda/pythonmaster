d4a8644ba235811ea57fbbc2d93b05c118cb6f39
Alexispoveda <alexis.mars20@hotmail.com>
2019-11-29 07:18:40 -0500

    Prob1 Alexis test

--- Prob1/datasaurus.py
+++ Prob1/datasaurus.py
@@ -2,6 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
+def leyenda(x,y):
+    correl = np.corrcoef(x,y)
+    r = correl[0][1]
+
+    return '''
+    Media de x: {0:.3f}
+    Media de y: {1:.3f}
+    Varianza de x: {2:.3f}
+    Varianza de y: {3:.3f}
+    Correlacion de X e Y: {4:.3f}'''.format(np.mean(x),np.mean(y),np.std(x),np.std(y),r)
+
 color = [
     'sienna',
     'tan',
@@ -14,13 +25,14 @@ color = [
     'salmon',
     'purple', 
     'tomato', 
-    'teal'
-]
-
+    'teal']
 imagenes = []
 Dicimagenes = {}
 
-with open('Prob1\problema1\DatasaurusDozen.tsv', 'r') as f:
+#Laptop de Alexis
+with open('/Users/alexispoveda/Desktop/Python/TareaPythonCientifico/Archivos/ArchivosTarea3/problema1/DatasaurusDozen.tsv', 'r') as f:
+#Laptop de Gabo
+#with open('Prob1\problema1\DatasaurusDozen.tsv', 'r') as f:
     reader = csv.reader(f, delimiter='\t') 
     for row in reader:
         if row[0] not in imagenes:
@@ -30,23 +42,15 @@ with open('Prob1\problema1\DatasaurusDozen.tsv', 'r') as f:
 
 for elem in imagenes:
     listaP = []
-    with open('Prob1\problema1\DatasaurusDozen.tsv', 'r') as f:
+    #Laptop de Alexis
+    with open('/Users/alexispoveda/Desktop/Python/TareaPythonCientifico/Archivos/ArchivosTarea3/problema1/DatasaurusDozen.tsv', 'r') as f:
+    #Laptop de Gabo
+    #with open('Prob1\problema1\DatasaurusDozen.tsv', 'r') as f:
         reader = csv.reader(f, delimiter='\t') 
         for row in reader:
             if(elem==row[0]):
                 listaP.append([float(row[1]), float(row[2])])
         Dicimagenes[elem] = listaP
-
-def legenda(x,y):
-    correl = np.corrcoef(x,y)
-    r = correl[0][1]
-
-    return '''
-    Media de x: {0:.3f}
-    Media de y: {1:.3f}
-    Varianza de x: {2:.3f}
-    Varianza de y: {3:.3f}
-    Correlacion de X e Y: {4:.3f}'''.format(np.mean(x),np.mean(y),np.std(x),np.std(y),r)
 
 #Impresion del Dino    
 plt.figure()
@@ -64,14 +68,13 @@ plt.legend([
     'Varianza de y: ' + str(np.std(y)),
     'Correlacion de x e y: ' + str(np.corrcoef(x,y))
 ])
-plt.text(70,80, legenda(x,y))
+plt.text(70,80, leyenda(x,y))
 plt.title('Dino')
 plt.xlabel('X')
 plt.ylabel('Y')
 
-#plt.show()
-
 del Dicimagenes['dino']
+
 #Impresion de las demas imagenes
 plt.figure()
 
@@ -83,9 +86,9 @@ for i, img in enumerate(Dicimagenes):
         x.append(point[0])
         y.append(point[1])
     plt.scatter(x,y, color=color[i])
-    plt.text(70,0, legenda(x,y), fontsize=8)
+    plt.text(70,0, leyenda(x,y), fontsize=8)
     plt.title(img)
     plt.xlabel('X')
     plt.ylabel('Y')
-plt.subplots_adjust(wspace=0.5, hspace=0.5)
+plt.subplots_adjust(wspace=2, hspace=2)
 plt.show()
